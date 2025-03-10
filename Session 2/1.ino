bb514c1ba0ed5b8b7b7e967c16895b3537edf6d5
Mohammadali Dehghanian <mohammadali.d1383@gmail.com>
2025-03-10 15:03:16 +0330

    Update 1.ino

--- Session 2/1.ino
+++ Session 2/1.ino
@@ -1,1 +1,19 @@
+int inPin = 8;
+int ledPin = 2;
+int val = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(inPin, INPUT);
+}
 
+void loop() {
+  val = digitalRead(inPin);
+  if (val == HIGH)
+  {
+  digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+  digitalWrite(ledPin, LOW);
+  }
+}
